1e5cb1489944e3d431de52ccf960273a02d60fa8
JacksonMcsorley <71676794+JacksonMcsorley@users.noreply.github.com>
2024-04-02 19:32:56 -0400

    Update populatetable.sql

--- populatetable.sql
+++ populatetable.sql
@@ -1,4 +1,3 @@
--- Inserting data into ChainesHotels table
 INSERT INTO ChainesHotels (IDChaineHotel, NomChaineHotel, AdresseBureauCentral, EmailContact, TelephoneContact)
 VALUES
 (1, 'Marriott', 'Marriott Headquarters Address', 'marriott@example.com', '123-456-7890'),
@@ -22,18 +21,18 @@ VALUES
 (10, 'Westin', 'Westin Hotel 2', 'Westin Hotel 2 Address', 'westin2@example.com', '123-456-7890', 4);
 
 -- Inserting data into Chambres table
-INSERT INTO Chambres (IDChambre, IDHotel,NomChaineHotel, NomHotel, Prix, Capacite, Vue, Extensible, ProblemesDommages)
+INSERT INTO Chambres (IDChambre, IDHotel,NomChaineHotel, NomHotel, Prix, Capacite, Vue, Extensible, ProblemesDommages, Disponible, Client_arrived)
 VALUES
-(1, 1,'Marriott','Marriott Hotel 1', 150.00, 'Single', 'City View', TRUE, NULL),
-(2, 1,'Marriott','Marriott Hotel 2', 200.00, 'Double', 'Ocean View', FALSE, 'Minor damage to door'),
-(3, 2,'Super8','Super8 Hotel 1', 80.00, 'Single', 'Street View', TRUE, NULL),
-(4, 2,'Super8','Super8 Hotel 2', 120.00, 'Double', 'Pool View', FALSE, NULL),
-(5, 3,'Delta','Delta Hotel 1', 100.00, 'Single', 'City View', TRUE, NULL),
-(6, 3,'Delta','Delta Hotel 2', 150.00, 'Double', 'Mountain View', FALSE, NULL),
-(7, 4,'Sheraton','Sheraton Hotel 1', 200.00, 'Single', 'City View', TRUE, NULL),
-(8, 4,'Sheraton','Sheraton Hotel 2', 250.00, 'Double', 'Beach View', FALSE, 'None'),
-(9, 5,'Westin','Westin Hotel 1', 180.00, 'Single', 'City View', TRUE, 'Minor scratches on furniture'),
-(10, 5,'Westin','Westin Hotel 2', 220.00, 'Double', 'Lake View', FALSE, NULL);
+(1, 1,'Marriott','Marriott Hotel 1', 150.00, 5, 'City View', TRUE, NULL, TRUE, FALSE),
+(2, 1,'Marriott','Marriott Hotel 2', 200.00, 2, 'Ocean View', FALSE, 'Minor damage to door', TRUE, FALSE),
+(3, 2,'Super8','Super8 Hotel 1', 80.00, 4, 'Street View', TRUE, NULL, TRUE, FALSE),
+(4, 2,'Super8','Super8 Hotel 2', 120.00, 5, 'Pool View', FALSE, NULL, TRUE, FALSE),
+(5, 3,'Delta','Delta Hotel 1', 100.00, 2, 'City View', TRUE, NULL, TRUE, FALSE),
+(6, 3,'Delta','Delta Hotel 2', 150.00, 4, 'Mountain View', FALSE, NULL, TRUE, FALSE),
+(7, 4,'Sheraton','Sheraton Hotel 1', 200.00, 2, 'City View', TRUE, NULL, TRUE, FALSE),
+(8, 4,'Sheraton','Sheraton Hotel 2', 250.00, 4, 'Beach View', FALSE, 'None', TRUE, FALSE),
+(9, 5,'Westin','Westin Hotel 1', 180.00, 5, 'City View', TRUE, 'Minor scratches on furniture', TRUE, FALSE),
+(10, 5,'Westin','Westin Hotel 2', 220.00, 5, 'Lake View', FALSE, NULL, TRUE, FALSE);
 
 -- Inserting data into Clients table
 INSERT INTO Clients (IDClient, NomComplet, Adresse, NumeroSecuriteSociale, DateEnregistrement)
@@ -50,11 +49,12 @@ VALUES
 -- Inserting data into Reservations table
 INSERT INTO Reservations (IDReservation, IDClient, IDChambre, DateReservation, DateDebut, DateFin)
 VALUES
-(1, 1, 1, '2024-04-01', '2024-04-10', '2024-04-15'),
-(2, 2, 3, '2024-05-15', '2024-05-20', '2024-05-25');
+(1, 11111111, 1, '2024-04-01', '2024-04-10', '2024-04-15'),
+(2, 11111111, 3, '2024-05-15', '2024-05-20', '2024-05-25');
 
 -- Inserting data into Locations table
 INSERT INTO Locations (IDLocation, IDReservation, IDEmploye, DateLocation)
 VALUES
 (1, 1, 1, '2024-04-10'),
 (2, 2, 2, '2024-05-15');
+
